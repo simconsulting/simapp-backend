21e0a4c2cac82f08735a29308f5941795527222f
karimalik <karimkompissi@gmail.com>
2021-11-03 17:27:29 +0100

    change port

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
